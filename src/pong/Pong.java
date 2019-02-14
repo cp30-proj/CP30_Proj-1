730357559d95bf10848e43e61dfa49fd8006d25c
Patthew <Patthew@DESKTOP-7UAATA5>
2019-02-14 11:33:18 +0800

    v0.0
    first commit
    empty file

---
+++ src/pong/Pong.java
@@ -0,0 +1,17 @@
+/*
+ *  © 2019 by Patrick Matthew Chan
+ *  File: Pong.java
+ *  Package: pong
+ *  Description: The Pong class.
+ */
+package pong;
+/* @author Patrick Matthew J. Chan [LBYCP24-EQ1]*/
+public class Pong{
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Main Class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        
+    }
+}
